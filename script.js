eaa739cf4349a47c963f837abf1c209319908eeb
ChimkariweObuseh <145494543+ChimkariweObuseh@users.noreply.github.com>
2024-09-14 12:13:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,20 +11,20 @@ let loadingdata2 = document.querySelector('.loadingdata2');
 let toggle = document.querySelector('.darklighttoggle');
 let toggleimg = document.querySelector('.darklighttoggleimg');
 let light = true;
-
+ 
 const pokemonCount = 1025;
 let pokedex = {};
 let completedLoading;
-
+ 
 window.onload = async function() {
-
-
+ 
+ 
     for (let i = 1; i <= pokemonCount; i++) {
         await getPokemon(i);
-
+ 
         let pokemon = document.createElement('div');
         pokemon.id = i;
-
+ 
         setInterval(() => {
             if (pokemon.id === 100) {
                 completeLoading();
@@ -33,27 +33,27 @@ window.onload = async function() {
             }
         }, 100);
         pokemon.classList.add('pokemon-name');
-
-
+ 
+ 
         pokemon.innerText = `${i}` + '. ' + pokedex[i].name.toUpperCase();
-
+ 
         pokemon.addEventListener('click', function() {
             for (let j = 1; j <= pokemonCount; j++) {
                 document.querySelectorAll(`.pokemon-name`)[j - 1].style.background = '#e92f30';
                 document.querySelectorAll(`.pokemon-name`)[j - 1].style.boxShadow = 'inset 0px 0px 0px 10px #910F21';
                 document.querySelectorAll(`.pokemon-name`)[j - 1].style.color = 'black';
             }
-
-
-
+ 
+ 
+ 
             pokemon.style.background = '#910F21';
             pokemon.style.boxShadow = 'inset 0px 0px 0px 10px #AEB6B8';
             pokemon.style.color = '#E92F30';
-
-
-
-
-
+ 
+ 
+ 
+ 
+ 
             if (pokedex[i].name.toUpperCase() !== cardheader.innerText) {
                 pokemoncard.style.opacity = 0;
                 setTimeout(() => {
@@ -62,13 +62,13 @@ window.onload = async function() {
                     cardheader.innerText = pokedex[pokemon.id]['name'].toUpperCase();
                     cardheader.style.fontSize = '15px';
                     cardheader.style.fontSize = 'min(25vw, 25px)';
-
+ 
                     setTimeout(() => {
                         pokemoncard.style.opacity = 1;
                     }, 100);
                 }, 400);
             }
-
+ 
             if (pokedex[pokemon.id]['type2'] && pokedex[pokemon.id]['species']['evolves_from_species'] && pokedex[pokemon.id]['species']['evolves_from_species']['name']) {
                 description.innerText = pokedex[pokemon.id]['name'] + ', the ' + pokedex[pokemon.id]['type1'] + ' and ' + pokedex[pokemon.id]['type2'] + ' type Pokémon, and the evolved form of ' + pokedex[pokemon.id]['species']['evolves_from_species']['name'].toUpperCase() + '. ' + pokedex[pokemon.id]['desc'];
             } else if (!pokedex[pokemon.id]['type2'] && pokedex[pokemon.id]['species']['evolves_from_species'] && pokedex[pokemon.id]['species']['evolves_from_species']['name']) {
@@ -79,12 +79,12 @@ window.onload = async function() {
                 description.innerText = pokedex[pokemon.id]['name'] + ', the ' + pokedex[pokemon.id]['type1'] + ' type Pokémon. ' + pokedex[pokemon.id]['desc'];
             }
         });
-
-
-
-
+ 
+ 
+ 
+ 
         console.log(pokemon.id);
-
+ 
         setInterval(function() {
             if (pokemon.id >= 1024) {
                 console.log('worked');
@@ -92,39 +92,39 @@ window.onload = async function() {
                 clearInterval();
             }
         }, 100);
-
+ 
         menu.append(pokemon);
     }
 }
-
+ 
 async function getPokemon(num) {
     let url = `https://pokeapi.co/api/v2/pokemon/${num}`;
-
+ 
     try {
         let response = await fetch(url);
-
+ 
         if (!response.ok) {
             throw new Error('Failed to fetch data');
         }
         let pokemon = await response.json();
         // console.log(pokemon);
-
+ 
         let pokemonName = pokemon["name"];
         let pokemonType = pokemon["types"][0]["type"]["name"];
         let pokemonType2 = false;
-
+ 
         if (pokemon["types"][1] && pokemon["types"][1]["type"]) {
             pokemonType2 = pokemon["types"][1]["type"]["name"];
         } else {
             pokemonType2 = false;
         }
         let pokemonImg = pokemon["sprites"]["front_default"];
-
+ 
         res = await fetch(pokemon['species']['url']);
         let pokemonSpecies = await res.json();
-
+ 
         let pokemonDescription = '';
-
+ 
         if (pokemonSpecies['flavor_text_entries'][9] && pokemonSpecies['flavor_text_entries'][9]['language'] && pokemonSpecies['flavor_text_entries'][9]['language']['name'] == 'en') {
             pokemonDescription = pokemonSpecies['flavor_text_entries'][9]['flavor_text'];
         } else if (pokemonSpecies['flavor_text_entries'][0] && pokemonSpecies['flavor_text_entries'][0]['language'] && pokemonSpecies['flavor_text_entries'][0]['language']['name'] == 'en') {
@@ -137,14 +137,14 @@ async function getPokemon(num) {
                 }
             }
         }
-
+ 
         if (pokemonDescription == false) {
             pokemonDescription = 'No description available.';
         }
-
+ 
         // console.log(pokemonSpecies);
         // console.log(pokemonDescription);
-
+ 
         pokedex[num] = {
             name: pokemonName.toUpperCase(),
             type1: pokemonType,
@@ -160,16 +160,16 @@ async function getPokemon(num) {
     }
     // console.log(pokedex);
 }
-
-
+ 
+ 
 function updatePokemon() {
     pokemonimage.src = pokedex[this.id]['img'];
 }
-
+ 
 input.addEventListener("input", e => {
     let search = e.target.value.toLowerCase();
     let pokemon = document.querySelectorAll('.pokemon-name');
-
+ 
     for (let i = 0; i < pokemon.length; i++) {
         let pokemonName = pokemon[i].innerText.toLowerCase();
         if (pokemonName.includes(search)) {
@@ -179,7 +179,7 @@ input.addEventListener("input", e => {
         }
     }
 });
-
+ 
 function completeLoading() {
     menu.style.display = 'inline-block';
     pokemoncard.style.display = 'inline-block';
@@ -194,15 +194,15 @@ function completeLoading() {
     }, 1000);
     completedLoading = true;
 }
-
+ 
 toggle.addEventListener('click', function() {
     if (!light) {
-        document.body.style.background = "url('IMG_5524.png')";
+        document.body.style.background = "url('IMG5524.png')";
         document.body.style.backgroundSize = "cover";
         document.body.style.backgroundRepeat = "no-repeat";
         document.body.style.backgroundPosition = "center";
         document.body.style.backgroundAttachment = "fixed";
-
+ 
         headerframe.style.background = "black";
         description.style.background = "black";
         headerframe.style.color = "white";
@@ -210,15 +210,15 @@ toggle.addEventListener('click', function() {
         loadingdata.style.color = "white";
         loadingdata2.style.color = "white";
         toggleimg.style.filter = "invert(100%)";
-
+ 
         light = true;
     } else {
-        document.body.style.background = "url('IMG_5523.png')";
+        document.body.style.background = "url('IMG5523.png')";
         document.body.style.backgroundSize = "cover";
         document.body.style.backgroundRepeat = "no-repeat";
         document.body.style.backgroundPosition = "center";
         document.body.style.backgroundAttachment = "fixed";
-
+ 
         headerframe.style.background = "white";
         description.style.background = "white";
         headerframe.style.color = "#4E5A51";
@@ -226,11 +226,11 @@ toggle.addEventListener('click', function() {
         loadingdata.style.color = "black";
         loadingdata2.style.color = "black";
         toggleimg.style.filter = "invert(0%)";
-
+ 
         light = false;
     }
 });
-
+ 
 setInterval(() => {
     if (completedLoading) {
         if (window.innerWidth <= 500) {
@@ -243,7 +243,7 @@ setInterval(() => {
         }
     }
 }, 100);
-
+ 
 function displayErrorMessage(str) {
     loadingdata.style.color = "red";
     loadingdata.innerText = str;
